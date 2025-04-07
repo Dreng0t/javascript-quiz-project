1a3ac3662d1f91bc95ebd92e1ee3627ae75d1b69
Dreng0t <louischicot4@gmail.com>
2025-04-07 14:58:33 +0100

    First draught

--- src/question.js
+++ src/question.js
@@ -4,4 +4,17 @@ class Question {
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
+    constructor(text, choice, answer, difficulty){
+        this.text = text;
+        this.choices = choice;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)); // Pick a random index from 0 to i
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]]; // Swap
+        }
+    }
 }
